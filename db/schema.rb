b67275d9d81de5ae1e8eedd7d2e48cd16c00890f
PuckHead <PuckHead@me.com>
2011-12-28 01:12:13 -0800

    Added Microposts reasource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111228090224) do
+ActiveRecord::Schema.define(:version => 20111228090900) do
 
   create_table "microsposts", :force => true do |t|
     t.string   "content"
